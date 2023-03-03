0f29f46d9105be5da6f9987f805e328f5d510a24
chungld2 <shootingstar.ldc@gmail.com>
2023-03-03 18:01:21 +0700

    use call replace transfer

---
+++ contracts/token/MoonToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract MoonToken is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant EMPLOYEE_ROLE = keccak256("EMPLOYEE_ROLE");
+
+    constructor() ERC20("MoonToken", "MTK") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
