556ad7f19000cf9156ee0164897181e957943fc6
chungld2 <shootingstar.ldc@gmail.com>
2023-03-06 10:36:21 +0700

    create

--- contracts/MoonVault.sol
+++ contracts/MoonVault.sol
@@ -5,5 +5,8 @@ import "../contracts/common/Initializer.sol";
 import "../contracts/common/Ownable.sol";
 
 contract MoonVault is Initializer, Ownable {
-    function initializer() public initialize {}
+
+    function initializer() public initialize {
+
+    }
 }
--- contracts/token/MoonToken.sol
+++ contracts/token/MoonToken.sol
@@ -8,6 +8,11 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract MoonToken is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant EMPLOYEE_ROLE = keccak256("EMPLOYEE_ROLE");
+    uint256 private constant CAP = 20000000000e18;
+
+    uint256 private _totalLock;
+    mapping(address => uint256) private _locks;
+    mapping(address => uint256) private _lastUnlockBlock;
 
     constructor() ERC20("MoonToken", "MTK") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
@@ -17,5 +22,74 @@ contract MoonToken is ERC20, ERC20Burnable, AccessControl {
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+    }
+
+    function cap() public pure returns (uint256){
+        return CAP;
+    }
+
+    function unlockedSupply() external view returns (uint256) {
+        return totalSupply().sub(totalLock());
+    }
+
+    function totalLock() public view returns (uint256) {
+        return _totalLock;
+    }
+
+    function totalBalanceOf(address _account) external view returns (uint256) {
+        return _locks[_account].add(balanceOf(_account));
+    }
+
+    function lockOf(address _account) external view returns (uint256) {
+        return _locks[_account];
+    }
+
+    function lastUnlockBlock(address _account) external view returns (uint256) {
+        return _lastUnlockBlock[_account];
+    }
+
+    function lock(address _account, uint256 _amount) external onlyOwner {
+        require(_account != address(0), "no lock to address(0)");
+        require(_amount <= balanceOf(_account), "no lock over balance");
+
+        _transfer(_account, address(this), _amount);
+
+        _locks[_account] = _locks[_account].add(_amount);
+        _totalLock = _totalLock.add(_amount);
+
+        if (_lastUnlockBlock[_account] < startReleaseBlock) {
+            _lastUnlockBlock[_account] = startReleaseBlock;
+        }
+
+        emit Lock(_account, _amount);
+    }
+
+    function canUnlockAmount(address _account) public view returns (uint256) {
+        // When block number less than startReleaseBlock, no ALPACAs can be unlocked
+        if (block.number < startReleaseBlock) {
+            return 0;
+        }
+        // When block number more than endReleaseBlock, all locked ALPACAs can be unlocked
+        else if (block.number >= endReleaseBlock) {
+            return _locks[_account];
+        }
+        // When block number is more than startReleaseBlock but less than endReleaseBlock,
+        // some ALPACAs can be released
+        else
+        {
+            uint256 releasedBlock = block.number.sub(_lastUnlockBlock[_account]);
+            uint256 blockLeft = endReleaseBlock.sub(_lastUnlockBlock[_account]);
+            return _locks[_account].mul(releasedBlock).div(blockLeft);
+        }
+    }
+
+    function unlock() external {
+        require(_locks[msg.sender] > 0, "no locked");
+        uint256 amount = canUnlockAmount(msg.sender);
+
+        _transfer(address(this), msg.sender, amount);
+        _locks[msg.sender] = _locks[msg.sender].sub(amount);
+        _lastUnlockBlock[msg.sender] = block.number;
+        _totalLock = _totalLock.sub(amount);
     }
 }
