454e581fcc01b1a8c0093c5ee64ee8fb025090b8
liquidator <chungld@moonlab.asia>
2023-11-13 13:57:11 +0700

    update hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,14 +2,13 @@ import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 require('@openzeppelin/hardhat-upgrades');
 
-const {privateKey} = require('./secret.json');
-
 const config: HardhatUserConfig = {
     defaultNetwork: 'hardhat',
     networks: {
         hardhat: {},
         development: {
-            url: 'http://127.0.0.1:8545'
+            url: 'http://127.0.0.1:8545',
+            accounts: ['0x7abcb7a752746dc534d8f79294fac8308bc31e3967d560f504a828209aed9d3d']
         },
         quorum: {
             url: 'http://127.0.0.1:22000'
@@ -18,13 +17,13 @@ const config: HardhatUserConfig = {
             url: "https://data-seed-prebsc-1-s1.binance.org:8545",
             chainId: 97,
             gasPrice: 20000000000,
-            accounts: [privateKey]
+            accounts: ['0x7abcb7a752746dc534d8f79294fac8308bc31e3967d560f504a828209aed9d3d']
         },
         mainnet: {
             url: "https://bsc-dataseed.binance.org/",
             chainId: 56,
             gasPrice: 20000000000,
-            accounts: [privateKey]
+            accounts: ['0x7abcb7a752746dc534d8f79294fac8308bc31e3967d560f504a828209aed9d3d']
         }
     },
 
